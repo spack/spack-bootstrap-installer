fc66edc3264fb30723ef7d5bf3b7b206a48ef56b
lou.lawrence@kitware.com <lou.lawrence@kitware.com>
2020-12-15 16:50:35 -0500

    preliminary batch script for PATH

---
+++ setup.bat
@@ -0,0 +1,3 @@
+setx /M PATH=c:/Program Files/Spack\ 0.16.0/spack-0.16.0;c:/Program Files/Spack\ 0.16.0/Python-3.9.0;${PATH}
+export SPACK_ROOT=c:/Program Files/Spack\ 0.16.0/spack-0.16.0
+/c/Program\ Files/Spack\ 0.16.0/Python-3.9.0/python.exe -m pip install pywin32
