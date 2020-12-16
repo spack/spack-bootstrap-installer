348d3e91ef9eb424bc875fa8f4fcf98ebe1bc9a2
lou.lawrence@kitware.com <lou.lawrence@kitware.com>
2020-12-16 17:24:42 -0500

    component install and WIX patch

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
-setx /M PATH=c:/Program Files/Spack\ 0.16.0/spack-0.16.0;c:/Program Files/Spack\ 0.16.0/Python-3.9.0;${PATH}
-export SPACK_ROOT=c:/Program Files/Spack\ 0.16.0/spack-0.16.0
-/c/Program\ Files/Spack\ 0.16.0/Python-3.9.0/python.exe -m pip install pywin32
+setx PATH "%ProgramFiles%/Spack 0.16.0/spack-0.16.0;%ProgramFiles%/Spack 0.16.0/Python-3.9.0;${PATH}"
+set SPACK_ROOT="%ProgramFiles%/Spack 0.16.0/spack-0.16.0"
+"%ProgramFiles%/Spack 0.16.0/Python-3.9.0/python.exe" -m pip install pywin32
