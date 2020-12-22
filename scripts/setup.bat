ec31861155a687ad9beb9a9292ef1023662a4caf
lou.lawrence@kitware.com <lou.lawrence@kitware.com>
2020-12-22 12:55:33 -0500

    Setup spack environment

---
+++ scripts/setup.bat
@@ -0,0 +1,1 @@
+"%ProgramFiles%/Spack 0.16.0/Python-3.9.0/python.exe" -m pip install pywin32
--- scripts/spack_cmd.bat
+++ scripts/spack_cmd.bat
@@ -1,9 +1,6 @@
 @ECHO OFF
-pushd %~dp0
-set PATH="%spackinstdir%/Python-3.9.0;%PATH%"
-set SPACK_ROOT="%spackinstdir%/spack-0.16.0"
-set spack="%CD%/spack.bat"
-popd
+set PATH="%CD%\scripts\;%CD%\Python-3.9.0\;%PATH%"
+set SPACK_ROOT="%CD%\spack-0.16.0\"
 @echo **********************************************************************
 @echo ** Spack Package Manager
 @echo ** ^(c^) 2020 Lawrence Livermore National Laboratories
